ae21f1d19630b7ae933c91e6f8955744227342af
Matheus Kinchin <mathkinchin@gmail.com>
2026-02-12 15:13:50 -0300

    fix

--- src/NivelIntermediario/Desafio3/Ninja.java
+++ src/NivelIntermediario/Desafio3/Ninja.java
@@ -11,6 +11,7 @@ public class Ninja {
         System.out.println("Essas são as informações do Ninja: ");
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
+        System.out.println("Missão: " + missao);
         System.out.println("Nivel Dificuldade: " + nivelDificuldade);
         System.out.println("Status da Missão: " + statusMissao);
     }
--- src/NivelIntermediario/Desafio3/Uchiha.java
+++ src/NivelIntermediario/Desafio3/Uchiha.java
@@ -5,15 +5,13 @@ public class Uchiha extends Ninja {
     String habilidadeEspecial;
 
     public void mostrarHabilidadeEspecial() {
-        System.out.println(habilidadeEspecial);
+        System.out.println("Habilidade Especial " + habilidadeEspecial);
     }
 
+    @Override
     public void mostrarInformacoes() {
-        System.out.println("Essas são as informações do Ninja: ");
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
-        System.out.println("Nivel Dificuldade: " + nivelDificuldade);
-        System.out.println("Status da Missão: " + statusMissao);
+        super.mostrarInformacoes();
+        mostrarHabilidadeEspecial();
     }
 
 
